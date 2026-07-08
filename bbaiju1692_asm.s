a5ee8d8962410779b0ad30d70bc0bdad344cf7db
Blessy Baiju <bbaiju1692@conestogac.on.ca>
2026-07-08 17:48:06 -0400

    Added Lab 6 template files by bbaiju1692

--- bbaiju1692_asm.s
+++ bbaiju1692_asm.s
@@ -1,143 +1,86 @@
-@ bbaiju1692_asm.s Data section - initialized values
-.data
-.align 3    @ This alignment is critical - to access our "huge" value, it must
-            @ be 64 bit aligned
-huge:   .octa 0xAABBCCDDDDCCBBFF
-big:    .word 0xAAEEBBFF
-num:    .byte 0xAB
-str2:   .asciz "Guten Tag!"
-count:  .word 12345                     @ This is an initialized 32 bit value
-@ End of new data section
-
-
-
 @ Test code for my own new function called from C
+
 @ This is a comment. Anything after an @ symbol is ignored.
 @@ This is also a comment. Some people use double @@ symbols. 
-    .code   16
-    .text
-    .align  2
-    .syntax unified
-    .global bbaiju1692_add_test
-    .code   16
-    .thumb_func
-    .type   bbaiju1692_add_test, %function
-@ Function Declaration : int bbaiju1692_add_test (int x, int y)
+
+
+    .code   16              @ This directive selects the instruction set being generated. 
+                            @ The value 16 selects Thumb, with the value 32 selecting ARM.
+
+    .text                   @ Tell the assembler that the upcoming section is to be considered
+                            @ assembly language instructions - Code section (text -> ROM)
+
+@@ Function Header Block
+    .align  2               @ Code alignment - 2^n alignment (n=2)
+                            @ This causes the assembler to use 4 byte alignment
+
+    .syntax unified         @ Sets the instruction set to the new unified ARM + THUMB
+                            @ instructions. The default is divided (separate instruction sets)
+
+    .global bbaiju1692_lab6        @ Make the symbol name for the function visible to the linker
+
+    .code   16              @ 16bit THUMB code (BOTH .code and .thumb_func are required)
+    .thumb_func             @ Specifies that the following symbol is the name of a THUMB
+                            @ encoded function. Necessary for interlinking between ARM and THUMB code.
+
+    .type   bbaiju1692_lab6, %function   @ Declares that the symbol is a function (not strictly required)
+
+@ Function Declaration : int bbaiju1692_lab6(int x, int y)
 @
 @ Input: r0, r1 (i.e. r0 holds x, r1 holds y)
 @ Returns: r0
 @ 
-@ Here is the actual bbaiju1692_add_test function
-bbaiju1692_add_test:
-    bkpt
-    @ Load the addresses of each of our items
-    ldr r0, =num
-    ldr r0, =big
-    ldr r0, =huge
-    ldr r0, =str2
-    ldr r2, =str2			@ Load the address of str2 and store it in r2
-    ldrb r0, [r2]			@ Load the value stored at the address str2 as a byte
-    ldr r2, =str2			@ Load the address of str2 and store it in r2
-    ldr r0, [r2]			@ Load the value stored at the address str2 as a word
-    ldr r2, =num			@ Load the address of num and store it in r2
-    ldrb r0, [r2]			@ Load the value stored at the address num
-    ldr r2, =big			@ Load the address of big
-    ldr r0, [r2]			@ Load the value of big
-    ldr r2, =huge			@ Load the address of huge
-    ldrd r0, r1, [r2]		@ Load the value of huge
-    push {lr}           @ Save LR because we will call another function
-    add r0, r0, r1      @ Add x + y, result is in r0
-    push {r0}           @ Save the addition result
-    mov r0, r2          @ Move delay parameter (r2) into r0 for busy_delay
-    bl busy_delay       @ Call busy_delay with the delay value
-    pop {r0}            @ Restore the addition result
-    pop {pc}            @ Restore and return
-    .size   bbaiju1692_add_test, .- bbaiju1692_add_test
+
+@ Here is the actual bbaiju1692_lab6 function
+bbaiju1692_lab6:
+    push {lr}
+
+    @ These lines just show that the code is working
+    ldr r0, =0xFFFFFF
+    bl busy_delay
+
+    pop {lr}
+    bx lr                           @ Return (Branch eXchange) to the address in the link register (lr) 
+    .size   bbaiju1692_lab6, .-bbaiju1692_lab6    @@ - symbol size (not strictly required, but makes the debugger happy)
 
 
-@@ Function Header Block
-    .align  2               @ Code alignment is 2^n alignment (n=2)
-    .syntax unified         @ Sets the instruction set to the unified ARM + THUMB
-    .global bbaiju1692_a2   @ Make the symbol name for the function visible to the linker
-    .code   16              @ 16bit THUMB code (BOTH .code and .thumb_func are required)
-    .thumb_func             @ Specifies that the following symbol is the name of a THUMB
-    .type   bbaiju1692_a2, %function   @ Declares that the symbol is a function (not strictly required)
-@ Function Declaration : int bbaiju1692_a2 (int num, int wait)
+.global bbaiju1692_a3
+.type   bbaiju1692_a3, %function
+
+@ Function Declaration: int bbaiju1692_a3(char *pattern_ptr)
 @
-@ Input: r0 holds num (number of repeats), r1 holds wait (delay value)
-@ Returns: r0 holds the total count of LED toggles
-@ 
-@ Here is the assignment 2 assembly function
-
-bbaiju1692_a2:
-    push  {r4, r5, r6, r7, lr}   @ Save registers + link register in ONE push
-    mov   r4, r0                 @ r4 = num (how many times to repeat the full cycle)
-    mov   r5, r1                 @ r5 = wait (delay value between each LED toggle)
-    mov   r6, #0                 @ r6 = total toggle counter, starts at 0
-
-RepeatLoop:
-    cmp   r4, #0                 @ Check if num (repeats left) is zero
-    beq   DoneRepeat             @ If zero, all repeats finished, go to the end
-
-    mov   r7, #0               @ r7 = current LED index, start at LED 3 (first LED)
-
-LedLoop:
-    cmp   r7, #8               @ Check if we have gone past the last LED (index 10)
-    beq   EndLedLoop             @ If so, this cycle of 8 LEDs is done
-
-    mov   r0, r7                 @ Move current LED index into r0 for the call
-    bl    BSP_LED_Toggle         @ Toggle this LED (ON if OFF, OFF if ON)
-    add   r6, r6, #1             @ Add 1 to our total toggle counter
-
-    mov   r0, r5                 @ Move saved wait value into r0 for busy_delay
-    bl    busy_delay             @ Wait the requested delay before the next LED
-
-    add   r7, r7, #1             @ Move to the next LED index
-    b     LedLoop                @ Go back and check the next LED
-
-EndLedLoop:
-    sub   r4, r4, #1             @ Subtract 1 from repeat counter (one cycle done)
-    b     RepeatLoop             @ Go back and check if more repeats are needed
-
-DoneRepeat:
-    mov   r0, r6                 @ Put total toggle count into r0 to return it
-    pop   {r4, r5, r6, r7, pc}   @ Restore registers and return (lr -> pc) in ONE pop
-    .size   bbaiju1692_a2, .- bbaiju1692_a2   @ symbol size (makes debugger happy)
-
-
-.global bbaiju1692_string_test
-@ Function Declaration : int bbaiju1692_string_test(char *p)
-@
-@ Input: r0 (i.e. r0 a pointer to a byte array)
+@ Input: r0 (i.e. r0 is a pointer to the first character of the pattern)
 @ Returns: r0
 @ 
-@ Here is the actual function
-bbaiju1692_string_test:
-StringLoop:
-    ldrb r1, [r0]                    @ Dereference the character r0 points to
-    cmp r1, #0                       @ Check if that value is zero
-    beq OutLabel                     @ If it is, branch out
-    add r0, r0, #1                   @ Add one to R0
-    b StringLoop                     @ Branch back to string loop
-OutLabel:
-    bx lr                            @ Return the value of R0
-    .size   bbaiju1692_string_test, .-bbaiju1692_string_test
-@ Function Declaration : int busy_delay(int cycles)
+
+@ Here is the function
+bbaiju1692_a3:
+
+    bx lr
+    .size   bbaiju1692_a3, .-bbaiju1692_a3
+
+@ Function Declaration: int busy_delay(int cycles)
 @
-@ Input: r0 (i.e. r0 holds number of cycles to delay)
+@ Input: r0 (i.e. r0 is how many cycles to delay)
 @ Returns: r0
 @ 
-@ Here is the actual function. DO NOT MODIFY THIS FUNCTION.
+
+@ Here is the actual function. DO NOT MODIFY THIS FUNCTION
 busy_delay:
     push {r6}
     mov r6, r0
-delay_label:
-    subs r6, r6, #1
-    bge delay_label
-    mov r0, #0                      @ Always return zero (success)
+
+    d3lay_loop:
+        subs r6, r6, #1
+        bge d3lay_loop
+
+        mov r0, #0      @ Return zero (success)
+
     pop {r6}
-    bx lr                           @ Return (Branch eXchange) to the address in the link register (lr)
+    bx lr               @ Return to calling function
 
-@Assembly file ended by single .end directive on its own line
 
+@ Assembly file ended by single .end directive on its own line
 .end
+
+Things past the end directive are not processed, as you can see here.
--- bbaiju1692_hook.c
+++ bbaiju1692_hook.c
@@ -1,88 +1,59 @@
 /*
  *  C to assembler menu hook
+ *
+ *  Modified by bbaiju1692
  * 
  */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <ctype.h>
+
 #include "common.h"
 
-int bbaiju1692_add_test(int x, int y, uint32_t delay);
+int bbaiju1692_lab6(int x, int y);
 
-void AddTest(int action)
+void Lab6_bbaiju1692(int action)
 {
+
   if(action==CMD_SHORT_HELP) return;
   if(action==CMD_LONG_HELP) {
-    printf("Addition Test\n\n"
-       "This command tests new addition function by bbaiju1692\n"
-       );
+    printf("Lab 6\n\n"
+	   "This command tests new lab 6 function by bbaiju1692\n"
+	   );
+
     return;
   }
-
-  uint32_t delay;
-  int fetch_status;
-  fetch_status = fetch_uint32_arg(&delay);
-  if(fetch_status) {
-    // Use a default delay value
-    delay = 0xFFFFFF;
-  }
-
-  printf("bbaiju1692_add_test returned: %d\n", bbaiju1692_add_test(99, 87, delay) );
+  printf("bbaiju1692_lab6 returned: %d\n", bbaiju1692_lab6(99, 87) );
 }
 
-ADD_CMD("bbaiju1692_add", AddTest,"Test the new add function")
+ADD_CMD("bbaiju1692_lab6", Lab6_bbaiju1692,"Test the new lab 6 function")
 
-int bbaiju1692_string_test(char *p);
+int bbaiju1692_a3(char *pattern_ptr);
 
-void bbaiju1692_StringTest(int action)
+void A3_bbaiju1692(int action)
 {
+
   if(action==CMD_SHORT_HELP) return;
   if(action==CMD_LONG_HELP) {
-    printf("String Test\n\n"
-	   "This command tests new string function by bbaiju1692\n"
+    printf("Assignment 3 Test\n\n"
+	   "This is the A3 function by bbaiju1692\n"
 	   );
+
     return;
   }
+
   int fetch_status;
-  char *destptr;
-  fetch_status = fetch_string_arg(&destptr);
+  char *pattern;
+
+  fetch_status = fetch_string_arg(&pattern);
+
   if (fetch_status) {
     // Default logic goes here
+    pattern = "Test Pattern";
   }
-  printf("string_test returned: %d\n", bbaiju1692_string_test(destptr) );
+
+  printf("bbaiju1692_a3 returned: %d\n", bbaiju1692_a3(pattern) );
 }
 
-ADD_CMD("bbaiju1692_string", bbaiju1692_StringTest,"Test the new string function")
-
-int bbaiju1692_a2(int num, int wait);
-
-// Assignment 2 C Hook Function
-//
-void _bbaiju1692_Assignment2(int action)
-{
-  if(action==CMD_SHORT_HELP) return;
-  if(action==CMD_LONG_HELP) {
-    printf("Assignment 2\n\n"
-	   "This command triggers assignment 2 by bbaiju1692\n"
-	   );
-    return;
-  }
-  uint32_t num_input;
-  uint32_t wait_input;
-  int fetch_status;
-
-  fetch_status = fetch_uint32_arg(&num_input);
-  if(fetch_status) {
-  	// Use a default value
-  	num_input = 1;
-  }
-
-  fetch_status = fetch_uint32_arg(&wait_input);
-  if(fetch_status) {
-  	// Use a default value
-  	wait_input = 0xFFFFEF;
-  }
-
-  printf("bbaiju1692_a2 returned: %d\n", bbaiju1692_a2 (num_input, wait_input) );
-}
-ADD_CMD("bbaiju1692_a2", _bbaiju1692_Assignment2, "Assignment 2")
+ADD_CMD("bbaiju1692_a3", A3_bbaiju1692,"Run A3 for bbaiju1692")
